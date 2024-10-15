ffb7a9e5b74b3313a6b1528fa2462796d11044db
Eljan Peralta <eljan233@gmail.com>
2024-10-15 13:42:07 +0800

    Added Edits features for name and main goal

--- src/App.js
+++ src/App.js
@@ -21,9 +21,24 @@ function App() {
       <h2>Good Morning</h2>
       <h3><Time /></h3>
 
-      {name ? `Hello, ${name}!`:''}
+      {nameInputClassName == 'hidden' && (
+        <p>Hello, <button className='hover:text-blue-900' onClick={() => {
+          setNameInputClassName('block');
+          setMainGoalInputClassName('hidden');
+        }}>{name}!</button></p>
+      )}
 
-      <p>{mainGoal ? `Main Goal: ${mainGoal}`:''}</p>
+      {mainGoalInputClassName == 'hidden' && nameInputClassName == 'hidden' &&
+        (
+          <>
+            <h2>Main Goal</h2>
+            <p><button className='hover:text-blue-900' onClick={() => {
+              setMainGoalInputClassName('block');
+            }}>{`${mainGoal}`}</button></p>
+          </>
+        )
+      }
+
 
       <div className={nameInputClassName}>
         <p><label htmlFor="name">What should we call you?</label></p>
@@ -31,11 +46,13 @@ function App() {
         <button className='bg-blue-950 text-white px-2 py-1 hover:bg-blue-900' onClick={() => {
           setName(nameInput);
           setNameInputClassName('hidden');
-          setMainGoalInputClassName('block');
+          if (!name){
+            setMainGoalInputClassName('block');
+          }
         }}>Enter</button>
       </div>
-      
-      <div className={mainGoalInputClassName}>        
+
+      <div className={mainGoalInputClassName}>
         <p><label htmlFor="name">What is your main goal for Today?</label></p>
         <input type="text" className='py-1 px-2 border-b border-black' value={mainGoalInput} placeholder='Input your name' onChange={(e) => { setMainGoalInput(e.target.value) }} />
         <button className='bg-blue-950 text-white px-2 py-1 hover:bg-blue-900' onClick={() => {
@@ -43,7 +60,7 @@ function App() {
           setMainGoalInputClassName('hidden');
         }}>Enter</button>
       </div>
-      
+
     </div>
   );
 }
