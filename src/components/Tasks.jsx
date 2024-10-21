4d9c942eddecae74087b63b439f37b7c6e873a7c
Eljan Peralta <eljan233@gmail.com>
2024-10-21 21:35:07 +0800

    Added Delete button for tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -6,6 +6,8 @@ function Tasks() {
 
    const [showContainer, setShowContainer] = useState(false);
 
+   const [taskHoverIndex, setTaskHoverIndex] = useState(null);
+
    const handleAddTodo = (e) => {
       e.preventDefault();
       setTodos([...todos, { name: todoInput, checked: false }]);
@@ -22,28 +24,56 @@ function Tasks() {
             <div className="bg-black bg-opacity-60 p-5 py-3 w-72 text-left rounded-md">
                <h2 className="text-lg mb-3">Today</h2>
                {todos.map((todo, index) => (
-                  <label
-                     key={index}
-                     className="flex items-center"
-                  >
-                     <input
-                        type="checkbox"
-                        className={`mr-2`}
-                        checked={todo.checked}
-                        onChange={(e) => {
-                           const newTodos = [...todos];
-                           newTodos[index].checked = e.target.checked;
-                           setTodos(newTodos);
-                        }}
-                     />
-                     <span
-                        className={`${
-                           todo.checked ? `line-through text-gray-700` : ``
+                  <>
+                     <div
+                        className={`flex justify-between pl-1 ${
+                           taskHoverIndex === index ? "bg-gray-900" : ""
                         }`}
+                        onMouseEnter={() => {
+                           setTaskHoverIndex(index);
+                        }}
+                        onMouseLeave={() => {
+                           setTaskHoverIndex(null);
+                        }}
                      >
-                        {todo.name}
-                     </span>
-                  </label>
+                        <label
+                           key={index}
+                           className="flex items-center"
+                        >
+                           <input
+                              type="checkbox"
+                              className={`mr-2`}
+                              checked={todo.checked}
+                              onChange={(e) => {
+                                 const newTodos = [...todos];
+                                 newTodos[index].checked = e.target.checked;
+                                 setTodos(newTodos);
+                              }}
+                           />
+                           <span
+                              className={`${
+                                 todo.checked
+                                    ? `line-through text-gray-700`
+                                    : ``
+                              }`}
+                           >
+                              {todo.name}
+                           </span>
+                        </label>
+                        {taskHoverIndex === index && (
+                           <button
+                              className="bg-red-500 text-black px-1"
+                              onClick={() => {
+                                 const newArray = [...todos];
+                                 newArray.splice(index, 1);
+                                 setTodos(newArray);
+                              }}
+                           >
+                              X
+                           </button>
+                        )}
+                     </div>
+                  </>
                ))}
 
                <form
@@ -52,6 +82,7 @@ function Tasks() {
                >
                   <input
                      type="text"
+                     required
                      className="bg-transparent focus:outline-none mt-3"
                      placeholder="New Task"
                      value={todoInput}
